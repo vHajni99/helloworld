f156dff6fdff08037e16689671a470ee56899d3b
vHajni99 <43650343+vHajni99@users.noreply.github.com>
2018-09-27 21:33:13 +0200

    Add files via upload

---
+++ drillch3.cpp
@@ -0,0 +1,27 @@
+#include "std_lib_facilities.h"
+int main()
+{
+cout << "Please enter your first name (followed by 'enter'):\n";
+string first_name; 
+cin >> first_name; 
+cout << "Hello, " << first_name << "\n";
+cout << "Dear, " <<  first_name, <<":\n";
+cout <<"How are you? I am fine. I miss you. ":\n";
+string friend_name;
+cin >> friend_name;
+cout <<"Have you seen "<< friend_name <<" lately? >":\n";
+char friend_sex;
+cin >> friend_sex;
+if (friend_sex == f cout << "If you see "<< friend_name <<"please ask him to call me."
+if friend_sex == m cout << "If you see "<< friend_name <<" please ask her to call me."
+int age ;
+cin >> age ) ;
+"I hear you just had a birthday and you are "<< age << "years old.”;
+If age is 0 or less or 110 or more, , call simple_error("you're
+kidding!") using simple_error() from std_lib_facilities.h.
+If age<12 cout << "Next year you will be "<< age+1 <<". ";
+If age==17 cout << "Next year you will be able to vote.";
+If age>70 cout << "I hope you are enjoying retirement.";
+cout << "Yours sincerely, ":\n";
+cout <<"         ":\n";
+}
