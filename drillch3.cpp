9a728527ea51814281553e3727220b58342680da
vHajni99 <43650343+vHajni99@users.noreply.github.com>
2018-10-05 13:26:44 +0200

    Add files via upload

--- drillch3.cpp
+++ drillch3.cpp
@@ -1,27 +1,56 @@
 #include "std_lib_facilities.h"
+
 int main()
 {
-cout << "Please enter your first name (followed by 'enter'):\n";
-string first_name; 
-cin >> first_name; 
-cout << "Hello, " << first_name << "\n";
-cout << "Dear, " <<  first_name, <<":\n";
-cout <<"How are you? I am fine. I miss you. ":\n";
-string friend_name;
-cin >> friend_name;
-cout <<"Have you seen "<< friend_name <<" lately? >":\n";
-char friend_sex;
-cin >> friend_sex;
-if (friend_sex == f cout << "If you see "<< friend_name <<"please ask him to call me."
-if friend_sex == m cout << "If you see "<< friend_name <<" please ask her to call me."
-int age ;
-cin >> age ) ;
-"I hear you just had a birthday and you are "<< age << "years old.”;
-If age is 0 or less or 110 or more, , call simple_error("you're
-kidding!") using simple_error() from std_lib_facilities.h.
-If age<12 cout << "Next year you will be "<< age+1 <<". ";
-If age==17 cout << "Next year you will be able to vote.";
-If age>70 cout << "I hope you are enjoying retirement.";
-cout << "Yours sincerely, ":\n";
-cout <<"         ":\n";
+	//1.
+	cout << "Enter the name of the person you want to write to\n";
+	string first_name;
+	cin >> first_name;
+	cout << "Dear " << first_name << ",\n";
+	//2.
+	cout << "How are you?\nI am fine. I miss you.\nI forgot to do this, but I'm just in time\nAnd to be honest I'm not creative at all...\n";
+	//3.
+	cout << "Please give me a name of your friend!\n";
+	string friend_name;
+	cin >> friend_name;
+	cout << "Have you seen " << friend_name << " lately?\n";
+	//4.
+	char friend_sex = '0';
+	do
+	{
+		cout << "Is your friend male or female?\nGive me an f or an m (capital letters won't work)\n";
+		cin >> friend_sex;
+	}while(friend_sex != 'f' && friend_sex != 'm');
+	if (friend_sex == 'm')
+	{
+		cout << "If you see " << friend_name << " please ask him to call me.\n";
+}
+	if (friend_sex == 'f')
+	{
+		cout << "If you see " << friend_name << " please ask her to call me.\n";
+	}
+	//5.
+	int age;
+	cout << "PLease give me your age!\n";
+	cin >> age;
+	if (age > 0 && age < 110)
+	{
+		cout << "I hear you just had a birthday and you are " << age << " years old.\n";
+	} else 
+	{
+		simple_error("you're kidding!");
+	}
+	//6.
+	if (age < 12)
+	{
+		cout << "Next year you will be " << age + 1 << ".\n";
+	} else if (age == 17)
+	{
+		cout << "Next year you will be able to vote.\n";
+	} else if (age > 70)
+	{
+		cout << "I hope you are enjoying retirement.\n";
+	}
+	//7.
+	cout << "Yours sincerely,\n\n\nJohn";
 }
